bb3111651a105d7943841191f5bb88100dfb1280
Vincent Taverna <vinny@getlua.com>
2018-08-26 17:06:21 -0400

    Support for minified version of react-dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 export default function({ types: t }) {
   return {
     visitor: {
-      FunctionDeclaration(path) {
-        // Method where errors get handled for classes
-        if (path.node.id.name === 'createClassErrorUpdate') {
+      CallExpression(path) {
+        const { property } = path.node.callee
+        if (property && property.name === 'componentDidCatch') {
           // Path that leads to the `componentDidCatch` call arguments
-          const componentDidCatchSecondArgPath = path.get('body.body.5.consequent.body.0.expression.right.body.body.4.expression.arguments.1')
-
-          // Modify `componentDidCatch` second argument to add a single line
-          componentDidCatchSecondArgPath.replaceWithSourceString(`{
-            componentStack: stack !== null ? stack : '',
-            source: errorInfo.source,
-          }`)
+          const componentDidCatchSecondArgPath = path.get('arguments.1')
+          const { properties } = componentDidCatchSecondArgPath.node
+          const variableDeclaration = path.getStatementParent().container[1].declarations.find(n =>
+            n.type === 'VariableDeclarator' && n.init.property.name === 'stack'
+          )
+          if (properties && properties[0].key.name === 'componentStack' && variableDeclaration) {
+            const sourceObjName = variableDeclaration.init.object.name
+            // Modify `componentDidCatch` second argument to add a single line
+            componentDidCatchSecondArgPath.replaceWithSourceString(`{
+              componentStack: stack !== null ? stack : '',
+              source: ${sourceObjName}.source,
+            }`)
+          }
         }
       }
     }
